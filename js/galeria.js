19b4865c7cb483f7e2cfcf558657f11b339b6b30
jvvsistemas <jvvsistemas@gmail.com>
2026-01-21 15:43:29 -0600

    update geleria

--- js/galeria.js
+++ js/galeria.js
@@ -4,16 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const imagenes = [
     
     "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016209/1_b5vz3w.jpg",
-    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016212/3_ahebjd.jpg",
+    
     "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016215/7_ivqmz8.jpg",
     "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016213/8a_a2aqaw.jpg",
     "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016214/14_iklnl3.jpg",
     "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016218/19_jbzixh.jpg",
-    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016211/21_ntl97g.jpg",
     "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016216/24_jio6jc.jpg",
-    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016211/28_drn7iu.jpg",
-    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016212/30_j5mayq.jpg",
-    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769016212/29_czdnwo.jpg"
+    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769031476/e_mmwiqi.jpg",
+    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769031460/b_m74bfl.jpg",
+    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769031459/a_jonmf7.jpg",
+    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769031460/d_pax6pr.jpg",
+    "https://res.cloudinary.com/dnpevelfb/image/upload/v1769031459/c_e9y3rs.jpg"
   ];
 
   // duplicamos para efecto infinito
